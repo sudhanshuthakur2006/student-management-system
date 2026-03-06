476da6abb2ee91a903b091265bc809cac3167afc
sudhanshuthakur2006 <sudhanshuthakur09616@gmail.com>
2026-03-06 15:03:25 +0530

    Error resolve in student urls

--- student/urls.py
+++ student/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("",views.student),
-    path("add_student",views.add_student)
+    path("add_student/",views.add_student)
 ]
